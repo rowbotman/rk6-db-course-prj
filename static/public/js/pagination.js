e30cb98e6104dd99e7231bc4376a886b8c67af4b
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-05-30 02:36:12 +0300

    added stylelint config

--- static/public/js/pagination.js
+++ static/public/js/pagination.js
@@ -15,41 +15,6 @@ const pagination = (path, page) => {
         });
 };
 
-const redrawButtons = (num) => {
-    let buttons = document.getElementsByClassName('pagination__elem');
-    let i = 0;
-    let buttonsArray = Array.from(buttons);
-    for (let j = 0; j < buttonsArray.length; ++j) {
-        if (buttonsArray[j].innerText === '...') {
-            if (num - 2 !== 1) {
-                buttonsArray[j - 3].innerText = '...';
-                buttonsArray[j - 3].innerText = num - 3;
-            }
-            if (num + 2 !== buttonsArray.length) {
-                buttonsArray[j + 3].innerText = '...';
-                buttonsArray[j + 3].innerText = num - 3;
-            }
-            buttonsArray[j - 1].innerText = num - 1;
-            buttonsArray[j - 1].setAttribute('values', num - 1);
-            buttonsArray[j + 1].innerText = num + 1;
-            buttonsArray[j + 1].setAttribute('values', num + 1);
-            buttonsArray[j].innerText = num;
-            buttonsArray[j].setAttribute('values', num);
-        }
-    }
-    // Array.from(buttons).forEach((btn) => {
-    //     console.log(i, +btn.innerText, num);
-    //     if (isNaN(+btn.innerText)) {
-    //         btn.innerText = num;
-    //     }
-    //     if ((num - 1 <= +btn.innerText) && (+btn.innerText <= num + 1 )) {
-    //         btn.setAttribute('value', num - 1 + i);
-    //         btn.innerText = num - 1 + i;
-    //         console.log(num - 1 + i);
-    //     }
-    // });
-};
-
 const isExtra = (elem) => elem.className.search('extra') >= 0;
 
 const buttons = document.getElementsByClassName('pagination__elem');
@@ -81,17 +46,6 @@ if (btnArray.length > 1) {
             currentBtn.innerText = currentPage;
             pagination(url, currentPage).then(
                 (data) => {
-                    // const currentBtn = document.getElementsByClassName('pagination__elem_current')[0];
-                    // currentBtn.className = 'pagination__elem';
-                    // Array.from(buttons).some((btn) => {
-                    //     if (btn.getAttribute('value') === newPage) {
-                    //         btn.className = 'pagination__elem pagination__elem_current';
-                    //         if (btn.innerText === '...') {
-                    //             redrawButtons(+newPage);
-                    //         }
-                    //         return true;
-                    //     }
-
                     const titleRow = document.createElement('div');
                     titleRow.className = 'report__row';
                     child.appendChild(titleRow);
@@ -123,7 +77,6 @@ if (btnArray.length > 1) {
                     });
                 }
             );
-
         };
         btn.addEventListener('click', handler);
     });
