efb334e260de7a672fc88e79d840599d3561c61e
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-05-30 02:38:13 +0300

    pagination fixed. At present pagination without close pages

--- static/public/js/pagination.js
+++ static/public/js/pagination.js
@@ -15,59 +15,69 @@ const pagination = (path, page) => {
         });
 };
 
+const isExtra = (elem) => elem.className.search('extra') >= 0;
 
 const buttons = document.getElementsByClassName('pagination__elem');
-Array.from(buttons).forEach((btn) => {
-    const handler = () => {
-        let child = document.getElementsByClassName('report__items')[0];
-        child.innerHTML = '';
-        let url = window.location.pathname + window.location.search;
-        const pos = url.indexOf('?');
-        url += pos > 0 ? '&' : '?';
-        const newPage = btn.getAttribute('value');
-        pagination(url, newPage).then(
-            (data) => {
-                const currentBtn = document.getElementsByClassName('pagination__elem_current')[0];
-                currentBtn.className = 'pagination__elem';
-                Array.from(buttons).some((btn) => {
-                    if (btn.getAttribute('value') === newPage) {
-                        btn.className = 'pagination__elem pagination__elem_current';
-                        return true;
-                    }
-                });
-
-                const titleRow = document.createElement('div');
-                titleRow.className = 'report__row';
-                child.appendChild(titleRow);
-                let elem = document.createElement('div');
-                elem.className = 'report__elem report__elem_main';
-                elem.innerHTML = '№';
-                child.appendChild(elem);
-                for (let elemData in data[0]) {
-                    elem = document.createElement('div');
-                    elem.className = 'report__elem report__elem_main';
-                    elem.innerHTML = elemData;
-                    child.appendChild(elem);
+const btnArray = Array.from(buttons);
+if (btnArray.length > 1) {
+    btnArray.forEach((btn) => {
+        const handler = () => {
+            let child = document.getElementsByClassName('report__items')[0];
+            child.innerHTML = '';
+            let url = window.location.pathname + window.location.search;
+            const pos = url.indexOf('?');
+            url += pos > 0 ? '&' : '?';
+            let newPage = +btn.getAttribute('value');
+            const currentBtn = document.getElementsByClassName('pagination__elem_current')[0];
+            let currentPage = +currentBtn.getAttribute('value');
+            const lastPage = +document.getElementsByClassName(
+                'pagination__elem_extra')[1].getAttribute('value');
+            console.log(newPage);
+            if (!isExtra(btn)) {
+                if (newPage > 0) {
+                    currentPage += (currentPage < lastPage) ? newPage : 0;
+                } else {
+                    currentPage += (currentPage > 1) ? newPage : 0;
                 }
-                data.forEach((rowData, index) => {
-                    const row = document.createElement('div');
-                    row.className = 'report__row';
-                    child.appendChild(row);
-                    elem = document.createElement('div');
-                    elem.className = 'report__elem';
-                    elem.innerHTML = ++index + (newPage - 1) * 10;
-                    child.appendChild(elem);
-                    for (let elemData in rowData) {
-                        const elem = document.createElement('div');
-                        elem.className = 'report__elem';
-                        elem.innerHTML = rowData[elemData];
-                        child.appendChild(elem);
-                        elemData = +elemData;
-                    }
-                });
+            } else {
+                currentPage = newPage;
             }
-        );
-
-    };
-    btn.addEventListener('click', handler);
-});
+            currentBtn.setAttribute('value', currentPage);
+            currentBtn.innerText = currentPage;
+            pagination(url, currentPage).then(
+                (data) => {
+                    const titleRow = document.createElement('div');
+                    titleRow.className = 'report__row';
+                    child.appendChild(titleRow);
+                    let elem = document.createElement('div');
+                    elem.className = 'report__elem report__elem_main';
+                    elem.innerHTML = '№';
+                    child.appendChild(elem);
+                    for (let elemData in data[0]) {
+                        elem = document.createElement('div');
+                        elem.className = 'report__elem report__elem_main';
+                        elem.innerHTML = elemData;
+                        child.appendChild(elem);
+                    }
+                    data.forEach((rowData, index) => {
+                        const row = document.createElement('div');
+                        row.className = 'report__row';
+                        child.appendChild(row);
+                        elem = document.createElement('div');
+                        elem.className = 'report__elem';
+                        elem.innerHTML = ++index + (currentPage - 1) * 10;
+                        child.appendChild(elem);
+                        for (let elemData in rowData) {
+                            const elem = document.createElement('div');
+                            elem.className = 'report__elem';
+                            elem.innerHTML = rowData[elemData];
+                            child.appendChild(elem);
+                            elemData = +elemData;
+                        }
+                    });
+                }
+            );
+        };
+        btn.addEventListener('click', handler);
+    });
+}
